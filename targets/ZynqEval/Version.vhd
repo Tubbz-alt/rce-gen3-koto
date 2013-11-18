b316abfe233d43bc6970c91837e3314e4041fece
Ryan Herbst <rherbst@slac.stanford.edu>
2013-11-18 21:48:31 +0000

    git-svn-id: file:///afs/slac/g/reseng/svn/repos/cob_dpm/trunk/firmware@3525 0848cbd7-7569-0410-ab05-8cc0a0c67708

--- targets/ZynqEval/Version.vhd
+++ targets/ZynqEval/Version.vhd
@@ -18,7 +18,7 @@ package Version is
 
 constant FPGA_VERSION_C : std_logic_vector(31 downto 0) := x"F1000200"; -- MAKE_VERSION
 
-constant BUILD_STAMP_C : string := "Built Mon Nov 18 12:52:49 PST 2013 by rherbst";
+constant BUILD_STAMP_C : string := "Built Mon Nov 18 13:41:37 PST 2013 by rherbst";
 
 end Version;
 
