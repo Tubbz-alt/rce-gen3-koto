9f73921dc27b8eee294353ffefb5b1139de23600
Ryan Herbst <rherbst@slac.stanford.edu>
2014-06-05 22:48:36 +0000

    Checkpoint.
    git-svn-id: file:///afs/slac/g/reseng/svn/repos/cob_dpm/trunk/firmware@4898 0848cbd7-7569-0410-ab05-8cc0a0c67708

--- targets/DpmEmpty/Version.vhd
+++ targets/DpmEmpty/Version.vhd
@@ -18,7 +18,7 @@ package Version is
 
 constant FPGA_VERSION_C : std_logic_vector(31 downto 0) := x"DA000301"; -- MAKE_VERSION
 
-constant BUILD_STAMP_C : string := "DpmEmpty: Built Wed Jun  4 16:27:07 PDT 2014 by rherbst";
+constant BUILD_STAMP_C : string := "DpmEmpty: Built Thu Jun  5 12:39:49 PDT 2014 by rherbst";
 
 end Version;
 
