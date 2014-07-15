7bc3bc3ae1469ec50d1a290875cb102f6edcbcb7
Ryan Herbst <rherbst@slac.stanford.edu>
2014-07-15 05:32:58 +0000

    PPI Fix. Added user efuse register.
    git-svn-id: file:///afs/slac/g/reseng/svn/repos/cob_dpm/trunk/firmware@5065 0848cbd7-7569-0410-ab05-8cc0a0c67708

--- targets/ZynqEval/Version.vhd
+++ targets/ZynqEval/Version.vhd
@@ -16,9 +16,9 @@ USE ieee.std_logic_1164.ALL;
 
 package Version is
 
-constant FPGA_VERSION_C : std_logic_vector(31 downto 0) := x"F1000305"; -- MAKE_VERSION
+constant FPGA_VERSION_C : std_logic_vector(31 downto 0) := x"F1000306"; -- MAKE_VERSION
 
-constant BUILD_STAMP_C : string := "ZynqEval: Built Tue Jul  8 10:59:00 PDT 2014 by rherbst";
+constant BUILD_STAMP_C : string := "ZynqEval: Built Mon Jul 14 17:40:43 PDT 2014 by rherbst";
 
 end Version;
 
@@ -65,5 +65,6 @@ end Version;
 -- 05/14/2014 (0xF1000303): Compile test.
 -- 07/01/2014 (0xF1000304): Interrupt controller fix.
 -- 07/08/2014 (0xF1000305): Zero length header support
+-- 07/14/2014 (0xF1000306): Added e-fuse readback, PPI Fix
 -------------------------------------------------------------------------------
 
