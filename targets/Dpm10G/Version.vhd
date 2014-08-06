2a6d7e04c52fc3325d78de782ee12b867771357e
Ryan Herbst <rherbst@slac.stanford.edu>
2014-08-06 16:00:02 +0000

    10G test version.
    git-svn-id: file:///afs/slac/g/reseng/svn/repos/cob_dpm/trunk/firmware@5175 0848cbd7-7569-0410-ab05-8cc0a0c67708

--- targets/Dpm10G/Version.vhd
+++ targets/Dpm10G/Version.vhd
@@ -16,14 +16,15 @@ USE ieee.std_logic_1164.ALL;
 
 package Version is
 
-constant FPGA_VERSION_C : std_logic_vector(31 downto 0) := x"DB000001"; -- MAKE_VERSION
+constant FPGA_VERSION_C : std_logic_vector(31 downto 0) := x"DB000002"; -- MAKE_VERSION
 
-constant BUILD_STAMP_C : string := "Dpm10G: Built Thu Jul 10 11:34:05 PDT 2014 by rherbst";
+constant BUILD_STAMP_C : string := "Dpm10G: Built Tue Aug  5 17:52:05 PDT 2014 by rherbst";
 
 end Version;
 
 -------------------------------------------------------------------------------
 -- Revision History:
 -- 06/26/2014 (0xDB000001): First Version
+-- 08/05/2014 (0xDB000002): Second Version
 -------------------------------------------------------------------------------
 
