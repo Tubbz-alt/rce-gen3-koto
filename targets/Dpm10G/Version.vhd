aeac0811deddaba1023c99fbbd390ddf56e5289b
Ryan Herbst <rherbst@slac.stanford.edu>
2014-08-22 23:44:23 +0000

    Updates.
    git-svn-id: file:///afs/slac/g/reseng/svn/repos/cob_dpm/trunk/firmware@5349 0848cbd7-7569-0410-ab05-8cc0a0c67708

--- targets/Dpm10G/Version.vhd
+++ targets/Dpm10G/Version.vhd
@@ -16,9 +16,9 @@ USE ieee.std_logic_1164.ALL;
 
 package Version is
 
-constant FPGA_VERSION_C : std_logic_vector(31 downto 0) := x"DB000006"; -- MAKE_VERSION
+constant FPGA_VERSION_C : std_logic_vector(31 downto 0) := x"DB000007"; -- MAKE_VERSION
 
-constant BUILD_STAMP_C : string := "Dpm10G: Built Fri Aug 22 12:18:35 PDT 2014 by rherbst";
+constant BUILD_STAMP_C : string := "Dpm10G: Built Fri Aug 22 15:29:43 PDT 2014 by rherbst";
 
 end Version;
 
@@ -30,5 +30,6 @@ end Version;
 -- 08/18/2014 (0xDB000004): PPI Register Changes.
 -- 08/19/2014 (0xDB000005): BSI Change.
 -- 08/19/2014 (0xDB000006): CRC Fix.
+-- 08/19/2014 (0xDB000007): CRC Fix. #2
 -------------------------------------------------------------------------------
 
