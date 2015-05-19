79e90bcf55800fa151476e92934c4d5195215fac
Benjamin Reese <bareese@slac.stanford.edu>
2015-05-19 18:43:03 +0000

    DpmCore.xdc  - Added constraints for 10G Ethernet clock.
    AxisToPpi.vhd
     - Use genTKeep function to assign tKeep. Old way wouldn't synthesize in Vivado.
    
    PpiObPayload.vhd
     - Fix incomplete sensitivity list.
    
    PpiPgpLane.vhd
     - Don't output status words.
    
    PpiPgpArray.vhd
     - Rebuild with new PpiPgpLane and AxiLiteCrossbar.
    
    Dpm10G.vhd
     - Added PpiPgpArray and PGP links.
    
    Dpm10G.xdc
     - Updated constraints for new Dpm10G.vhd
    
    Updated source lists and Makefile.
    
    
    
    git-svn-id: file:///afs/slac/g/reseng/svn/repos/cob_dpm/trunk/firmware@7282 0848cbd7-7569-0410-ab05-8cc0a0c67708

--- targets/Dpm10G/Version.vhd
+++ targets/Dpm10G/Version.vhd
@@ -16,49 +16,50 @@ USE ieee.std_logic_1164.ALL;
 
 package Version is
 
-constant FPGA_VERSION_C : std_logic_vector(31 downto 0) := x"DB000025"; -- MAKE_VERSION
+constant FPGA_VERSION_C : std_logic_vector(31 downto 0) := x"DB000026"; -- MAKE_VERSION
 
-constant BUILD_STAMP_C : string := "Dpm10G: Vivado v2014.3 (x86_64) Built Wed Mar 11 16:39:56 PDT 2015 by rherbst";
+constant BUILD_STAMP_C : string := "Dpm10G: Vivado v2014.4 (x86_64) Built Mon May 18 14:20:19 PDT 2015 by bareese";
 
 end Version;
 
 -------------------------------------------------------------------------------
 -- Revision History:
--- 06/26/2014 (0xDB000001): First Version
--- 08/05/2014 (0xDB000002): Second Version
--- 08/17/2014 (0xDB000003): FIXED PPI
--- 08/18/2014 (0xDB000004): PPI Register Changes.
--- 08/19/2014 (0xDB000005): BSI Change.
--- 08/19/2014 (0xDB000006): CRC Fix.
--- 08/19/2014 (0xDB000007): CRC Fix. #2
--- 08/19/2014 (0xDB000008): CRC Fix. #3
--- 08/19/2014 (0xDB000009): Added clock constraints
--- 08/19/2014 (0xDB00000A): Added clock constraints
--- 08/19/2014 (0xDB00000B): Valid threshold change.
--- 08/19/2014 (0xDB00000C): Valid threshold change.
--- 08/19/2014 (0xDB00000d): Fifo and threshold fix.
--- 08/19/2014 (0xDB00000e): Export fixes. Proper simulation.
--- 08/19/2014 (0xDB00000f): state count fix.
--- 08/31/2014 (0xDB000010): PPI Bug Fix.
--- 09/24/2014 (0xDB000011): New RCE, new clocking.
--- 11/03/2014 (0xDB000012): RCE Updates, removed PPI crossbar, added debug.
--- 01/06/2015 (0xDB000013): AXI Stream FIFO fix.
--- 01/06/2015 (0xDB000014): Full library update.
--- 01/09/2015 (0xDB000015): Buffer size increase
--- 01/09/2015 (0xDB000016): Enabled inter-frame gap after pause frames
--- 01/09/2015 (0xDB000017): Added pause counters
--- 01/09/2015 (0xDB000018): Fix.
--- 01/13/2015 (0xDB000019): Pause RX FIX.
--- 01/13/2015 (0xDB00001A): Removed shifters.
--- 01/13/2015 (0xDB00001B): Import shift added.
--- 01/13/2015 (0xDB00001C): Export shift added.
--- 01/13/2015 (0xDB00001D): Register Fix.
--- 01/20/2015 (0xDB00001E): Added shift enables to fix first frame bug.
--- 01/20/2015 (0xDB00001F): Fixed pause frame generation.
--- 01/20/2015 (0xDB000020): Removed shifters.
--- 02/08/2015 (0xDB000021): Added counters.
--- 02/08/2015 (0xDB000022): Added counters.
--- 02/17/2015 (0xDB000023): PPI Core fix
--- 03/07/2015 (0xDB000024): PPI Flow Control Bug
+-- 05/15/2015 (0xDB000026): Added PPI PGP Array
 -- 03/07/2015 (0xDB000025): Size check
+-- 03/07/2015 (0xDB000024): PPI Flow Control Bug
+-- 02/17/2015 (0xDB000023): PPI Core fix
+-- 02/08/2015 (0xDB000022): Added counters.
+-- 02/08/2015 (0xDB000021): Added counters.
+-- 01/20/2015 (0xDB000020): Removed shifters.
+-- 01/20/2015 (0xDB00001F): Fixed pause frame generation.
+-- 01/20/2015 (0xDB00001E): Added shift enables to fix first frame bug.
+-- 01/13/2015 (0xDB00001D): Register Fix.
+-- 01/13/2015 (0xDB00001C): Export shift added.
+-- 01/13/2015 (0xDB00001B): Import shift added.
+-- 01/13/2015 (0xDB00001A): Removed shifters.
+-- 01/13/2015 (0xDB000019): Pause RX FIX.
+-- 01/09/2015 (0xDB000018): Fix.
+-- 01/09/2015 (0xDB000017): Added pause counters
+-- 01/09/2015 (0xDB000016): Enabled inter-frame gap after pause frames
+-- 01/09/2015 (0xDB000015): Buffer size increase
+-- 01/06/2015 (0xDB000014): Full library update.
+-- 01/06/2015 (0xDB000013): AXI Stream FIFO fix.
+-- 11/03/2014 (0xDB000012): RCE Updates, removed PPI crossbar, added debug.
+-- 09/24/2014 (0xDB000011): New RCE, new clocking.
+-- 08/31/2014 (0xDB000010): PPI Bug Fix.
+-- 08/19/2014 (0xDB00000f): state count fix.
+-- 08/19/2014 (0xDB00000e): Export fixes. Proper simulation.
+-- 08/19/2014 (0xDB00000d): Fifo and threshold fix.
+-- 08/19/2014 (0xDB00000C): Valid threshold change.
+-- 08/19/2014 (0xDB00000B): Valid threshold change.
+-- 08/19/2014 (0xDB00000A): Added clock constraints
+-- 08/19/2014 (0xDB000009): Added clock constraints
+-- 08/19/2014 (0xDB000008): CRC Fix. #3
+-- 08/19/2014 (0xDB000007): CRC Fix. #2
+-- 08/19/2014 (0xDB000006): CRC Fix.
+-- 08/19/2014 (0xDB000005): BSI Change.
+-- 08/18/2014 (0xDB000004): PPI Register Changes.
+-- 08/17/2014 (0xDB000003): FIXED PPI
+-- 08/05/2014 (0xDB000002): Second Version
+-- 06/26/2014 (0xDB000001): First Version
 -------------------------------------------------------------------------------
--- targets/Dpm10G/hdl/Dpm10G.vhd
+++ targets/Dpm10G/hdl/Dpm10G.vhd
@@ -2,179 +2,345 @@
 -- Dpm10G.vhd
 -------------------------------------------------------------------------------
 library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_1164.all;
 
 library UNISIM;
-use UNISIM.VCOMPONENTS.ALL;
+use UNISIM.VCOMPONENTS.all;
 use work.all;
 use work.RceG3Pkg.all;
 use work.StdRtlPkg.all;
 use work.AxiLitePkg.all;
 use work.AxiStreamPkg.all;
+use work.Gtx7CfgPkg.all;
+use work.Pgp2bPkg.all;
 
 entity Dpm10G is
+   generic (
+      TPD_G           : time                  := 1 ns;
+      PGP_LANES_G     : integer range 1 to 12 := 12;
+      PGP_LINE_RATE_G : real                  := 3.125E9);
    port (
 
       -- Debug
-      led          : out   slv(1 downto 0);
+      led : out slv(1 downto 0);
 
       -- I2C
-      i2cSda       : inout sl;
-      i2cScl       : inout sl;
+      i2cSda : inout sl;
+      i2cScl : inout sl;
 
       -- Ethernet
-      ethRxP       : in    slv(3 downto 0);
-      ethRxM       : in    slv(3 downto 0);
-      ethTxP       : out   slv(3 downto 0);
-      ethTxM       : out   slv(3 downto 0);
-      ethRefClkP   : in    sl;
-      ethRefClkM   : in    sl;
+      ethRxP     : in  slv(3 downto 0);
+      ethRxM     : in  slv(3 downto 0);
+      ethTxP     : out slv(3 downto 0);
+      ethTxM     : out slv(3 downto 0);
+      ethRefClkP : in  sl;
+      ethRefClkM : in  sl;
 
       -- RTM High Speed
-      --dpmToRtmHsP  : out   slv(11 downto 0);
-      --dpmToRtmHsM  : out   slv(11 downto 0);
-      --rtmToDpmHsP  : in    slv(11 downto 0);
-      --rtmToDpmHsM  : in    slv(11 downto 0);
+      dpmToRtmHsP : out slv(PGP_LANES_G-1 downto 0);
+      dpmToRtmHsM : out slv(PGP_LANES_G-1 downto 0);
+      rtmToDpmHsP : in  slv(PGP_LANES_G-1 downto 0);
+      rtmToDpmHsM : in  slv(PGP_LANES_G-1 downto 0);
 
       -- Reference Clocks
-      locRefClkP   : in    sl;
-      locRefClkM   : in    sl;
-      dtmRefClkP   : in    sl;
-      dtmRefClkM   : in    sl;
+      locRefClkP : in sl;
+      locRefClkM : in sl;
+      dtmRefClkP : in sl;
+      dtmRefClkM : in sl;
 
       -- DTM Signals
-      dtmClkP      : in    slv(1  downto 0);
-      dtmClkM      : in    slv(1  downto 0);
-      dtmFbP       : out   sl;
-      dtmFbM       : out   sl;
+      dtmClkP : in  slv(1 downto 0);
+      dtmClkM : in  slv(1 downto 0);
+      dtmFbP  : out sl;
+      dtmFbM  : out sl;
 
       -- Clock Select
-      clkSelA      : out   slv(1 downto 0);
-      clkSelB      : out   slv(1 downto 0)
-   );
+      clkSelA : out slv(1 downto 0);
+      clkSelB : out slv(1 downto 0)
+      );
 end Dpm10G;
 
 architecture STRUCTURE of Dpm10G is
 
-   constant TPD_C : time := 1 ns;
+   -- Constants
+   constant AXIL_CLK_FREQ_C    : real            := 125.0E6;
+   constant GTX_REFCLK_FREQ_C  : real            := 250.0E6;
+   constant PGP_GTX_CPLL_CFG_C : Gtx7CPllCfgType := getGtx7CPllCfg(GTX_REFCLK_FREQ_C, PGP_LINE_RATE_G);
 
-   -- Local Signals
-   signal axiClk             : sl;
-   signal axiClkRst          : sl;
-   signal sysClk125          : sl;
-   signal sysClk125Rst       : sl;
-   signal sysClk200          : sl;
-   signal sysClk200Rst       : sl;
+   signal locRefClk  : sl;
+   signal locRefClkG : sl;
+
+   -- DPM System clocks
+   signal sysClk125    : sl;
+   signal sysClk125Rst : sl;
+   signal sysClk200    : sl;
+   signal sysClk200Rst : sl;
+
+   -- AXI-Lite
+   signal axilClk            : sl;
+   signal axilClkRst         : sl;
    signal extAxilReadMaster  : AxiLiteReadMasterType;
    signal extAxilReadSlave   : AxiLiteReadSlaveType;
    signal extAxilWriteMaster : AxiLiteWriteMasterType;
    signal extAxilWriteSlave  : AxiLiteWriteSlaveType;
-   signal dmaClk             : slv(2 downto 0);
-   signal dmaClkRst          : slv(2 downto 0);
-   signal dmaState           : RceDmaStateArray(2 downto 0);
-   signal dmaObMaster        : AxiStreamMasterArray(2 downto 0);
-   signal dmaObSlave         : AxiStreamSlaveArray(2 downto 0);
-   signal dmaIbMaster        : AxiStreamMasterArray(2 downto 0);
-   signal dmaIbSlave         : AxiStreamSlaveArray(2 downto 0);
+
+   -- DMA
+   signal dmaClk      : slv(2 downto 0);
+   signal dmaClkRst   : slv(2 downto 0);
+   signal dmaState    : RceDmaStateArray(2 downto 0);
+   signal dmaObMaster : AxiStreamMasterArray(2 downto 0);
+   signal dmaObSlave  : AxiStreamSlaveArray(2 downto 0);
+   signal dmaIbMaster : AxiStreamMasterArray(2 downto 0);
+   signal dmaIbSlave  : AxiStreamSlaveArray(2 downto 0);
+
+   -- PGP
+   signal pgpClk           : sl;
+   signal pgpClkRst        : sl;
+   signal pgpTxIn          : Pgp2bTxInArray(PGP_LANES_G-1 downto 0);
+   signal pgpTxOut         : Pgp2bTxOutArray(PGP_LANES_G-1 downto 0);
+   signal pgpTxMasters     : AxiStreamQuadMasterArray(PGP_LANES_G-1 downto 0);
+   signal pgpTxSlaves      : AxiStreamQuadSlaveArray(PGP_LANES_G-1 downto 0);
+   signal pgpRxIn          : Pgp2bRxInArray(PGP_LANES_G-1 downto 0);
+   signal pgpRxOut         : Pgp2bRxOutArray(PGP_LANES_G-1 downto 0);
+   signal pgpRxMasterMuxed : AxiStreamMasterArray(PGP_LANES_G-1 downto 0);
+   signal pgpRxCtrl        : AxiStreamQuadCtrlArray(PGP_LANES_G-1 downto 0);
 
 begin
 
    --------------------------------------------------
    -- Core
    --------------------------------------------------
-   U_DpmCore: entity work.DpmCore 
+   U_DpmCore : entity work.DpmCore
       generic map (
-         TPD_G          => TPD_C,
+         TPD_G          => TPD_G,
          RCE_DMA_MODE_G => RCE_DMA_PPI_C,
          OLD_BSI_MODE_G => false,
          ETH_10G_EN_G   => true
-      ) port map (
-         i2cSda                   => i2cSda,
-         i2cScl                   => i2cScl,
-         ethRxP                   => ethRxP,
-         ethRxM                   => ethRxM,
-         ethTxP                   => ethTxP,
-         ethTxM                   => ethTxM,
-         ethRefClkP               => ethRefClkP,
-         ethRefClkM               => ethRefClkM,
-         clkSelA                  => clkSelA,
-         clkSelB                  => clkSelB,
-         sysClk125                => sysClk125,
-         sysClk125Rst             => sysClk125Rst,
-         sysClk200                => sysClk200,
-         sysClk200Rst             => sysClk200Rst,
-         axiClk                   => axiClk,
-         axiClkRst                => axiClkRst,
-         extAxilReadMaster        => extAxilReadMaster,
-         extAxilReadSlave         => extAxilReadSlave,
-         extAxilWriteMaster       => extAxilWriteMaster,
-         extAxilWriteSlave        => extAxilWriteSlave,
-         dmaClk                   => dmaClk,
-         dmaClkRst                => dmaClkRst,
-         dmaState                 => dmaState,
-         dmaObMaster              => dmaObMaster,
-         dmaObSlave               => dmaObSlave,
-         dmaIbMaster              => dmaIbMaster,
-         dmaIbSlave               => dmaIbSlave,
-         userInterrupt            => (others=>'0')
-      );
+         ) port map (
+            i2cSda             => i2cSda,
+            i2cScl             => i2cScl,
+            ethRxP             => ethRxP,
+            ethRxM             => ethRxM,
+            ethTxP             => ethTxP,
+            ethTxM             => ethTxM,
+            ethRefClkP         => ethRefClkP,
+            ethRefClkM         => ethRefClkM,
+            clkSelA            => clkSelA,
+            clkSelB            => clkSelB,
+            sysClk125          => sysClk125,
+            sysClk125Rst       => sysClk125Rst,
+            sysClk200          => sysClk200,
+            sysClk200Rst       => sysClk200Rst,
+            axiClk             => axilClk,
+            axiClkRst          => axilClkRst,
+            extAxilReadMaster  => extAxilReadMaster,
+            extAxilReadSlave   => extAxilReadSlave,
+            extAxilWriteMaster => extAxilWriteMaster,
+            extAxilWriteSlave  => extAxilWriteSlave,
+            dmaClk             => dmaClk,
+            dmaClkRst          => dmaClkRst,
+            dmaState           => dmaState,
+            dmaObMaster        => dmaObMaster,
+            dmaObSlave         => dmaObSlave,
+            dmaIbMaster        => dmaIbMaster,
+            dmaIbSlave         => dmaIbSlave,
+            userInterrupt      => (others => '0')
+            );
 
-   -- Empty AXI Slave
+
+   -------------------------------------------------------------------------------------------------
+   -- AXI-Lite Crossbar
+   -------------------------------------------------------------------------------------------------
    -- 0xA0000000 - 0xAFFFFFFF
-   U_AxiLiteEmpty: entity work.AxiLiteEmpty 
-      port map (
-         axiClk          => axiClk,
-         axiClkRst       => axiClkRst,
-         axiReadMaster   => extAxilReadMaster,
-         axiReadSlave    => extAxilReadSlave,
-         axiWriteMaster  => extAxilWriteMaster,
-         axiWriteSlave   => extAxilWriteSlave
-      );
+--   AxiLiteCrossbar_1 : entity work.AxiLiteCrossbar
+--      generic map (
+--         TPD_G              => TPD_G,
+--         NUM_SLAVE_SLOTS_G  => NUM_SLAVE_SLOTS_G,
+--         NUM_MASTER_SLOTS_G => NUM_MASTER_SLOTS_G,
+--         DEC_ERROR_RESP_G   => DEC_ERROR_RESP_G,
+--         MASTERS_CONFIG_G   => MASTERS_CONFIG_G)
+--      port map (
+--         axiClk           => axilClk,
+--         axiClkRst        => axilClkRst,
+--         sAxiWriteMasters => sAxiWriteMasters,
+--         sAxiWriteSlaves  => sAxiWriteSlaves,
+--         sAxiReadMasters  => sAxiReadMasters,
+--         sAxiReadSlaves   => sAxiReadSlaves,
+--         mAxiWriteMasters => mAxiWriteMasters,
+--         mAxiWriteSlaves  => mAxiWriteSlaves,
+--         mAxiReadMasters  => mAxiReadMasters,
+--         mAxiReadSlaves   => mAxiReadSlaves);
 
-
-   --------------------------------------------------
-   -- PPI Loopback
-   --------------------------------------------------
-   dmaClk      <= (others=>sysClk125);
-   dmaClkRst   <= (others=>sysClk125Rst);
-   dmaIbMaster <= dmaObMaster;
-   dmaObSlave  <= dmaIbSlave;
 
    --------------------------------------------------
    -- Top Level Signals
    --------------------------------------------------
-   led <= (others=>'0');
+   led <= (others => '0');
 
-   -- RTM High Speed
-   --dpmToRtmHsP : out   slv(11 downto 0);
-   --dpmToRtmHsM : out   slv(11 downto 0);
-   --rtmToDpmHsP : in    slv(11 downto 0);
-   --rtmToDpmHsM : in    slv(11 downto 0);
-
-   -- Reference Clocks
-   --locRefClkP   : in    sl;
-   --locRefClkM   : in    sl;
-   --dtmRefClkP   : in    sl;
-   --dtmRefClkM   : in    sl;
-
+   -------------------------------------------------------------------------------------------------
+   -- Clocking
+   -------------------------------------------------------------------------------------------------
    -- DTM Clock Signals
    U_DtmClkgen : for i in 0 to 1 generate
       U_DtmClkIn : IBUFDS
-         generic map ( DIFF_TERM => true ) 
+         generic map (DIFF_TERM => true)
          port map(
-            I      => dtmClkP(i),
-            IB     => dtmClkM(i),
-            O      => open
-         );
+            I  => dtmClkP(i),
+            IB => dtmClkM(i),
+            O  => open
+            );
    end generate;
 
    -- DTM Feedback
    U_DtmFbOut : OBUFDS
       port map(
-         O      => dtmFbP,
-         OB     => dtmFbM,
-         I      => '0'
-      );
+         O  => dtmFbP,
+         OB => dtmFbM,
+         I  => '0'
+         );
+
+   -- locRefClk drives PGP
+   U_LocRefClkIbufds : IBUFDS_GTE2
+      port map (
+         I     => locRefClkP,
+         IB    => locRefClkM,
+         CEB   => '0',
+         O     => locRefClk,
+         ODIV2 => open);
+
+   U_LocRefClkBufg : BUFG
+      port map (
+         I => locRefClk,
+         O => locRefClkG);
+
+   ClockManager7_1 : entity work.ClockManager7
+      generic map (
+         TPD_G              => TPD_G,
+         TYPE_G             => "MMCM",
+         INPUT_BUFG_G       => false,
+         FB_BUFG_G          => true,
+         RST_IN_POLARITY_G  => '1',
+         NUM_CLOCKS_G       => 1,
+         BANDWIDTH_G        => "OPTIMIZED",
+         CLKIN_PERIOD_G     => 4.0,
+         DIVCLK_DIVIDE_G    => 1,
+         CLKFBOUT_MULT_F_G  => 5.0,
+         CLKOUT0_DIVIDE_G   => 8,
+         CLKOUT0_RST_HOLD_G => 8)
+      port map (
+         clkIn     => locRefClkG,
+         rstIn     => axilClkRst,
+         clkOut(0) => pgpClk,
+         rstOut(0) => pgpClkRst);
+
+   dmaClk    <= (others => sysClk200);
+   dmaClkRst <= (others => sysClk200Rst);
+
+   -------------------------------------------------------------------------------------------------
+   -- PPI to PGP Array
+   -------------------------------------------------------------------------------------------------
+   PpiPgpArray_1 : entity work.PpiPgpArray
+      generic map (
+         TPD_G                   => TPD_G,
+         NUM_LANES_G             => PGP_LANES_G,
+         AXIL_BASE_ADDRESS_G     => X"A0000000",
+         AXIL_CLK_FREQ_G         => AXIL_CLK_FREQ_C,
+         RX_AXIS_ADDR_WIDTH_G    => 9,
+         RX_AXIS_PAUSE_THRESH_G  => 500,
+         RX_AXIS_CASCADE_SIZE_G  => 1,
+         RX_DATA_ADDR_WIDTH_G    => 12,
+         RX_HEADER_ADDR_WIDTH_G  => 9,
+         RX_PPI_MAX_FRAME_SIZE_G => 2048,
+         TX_PPI_ADDR_WIDTH_G     => 9,
+         TX_AXIS_ADDR_WIDTH_G    => 9,
+         TX_AXIS_CASCADE_SIZE_G  => 1)
+      port map (
+         ppiClk           => dmaClk(0),
+         ppiClkRst        => dmaClkRst(0),
+         ppiState         => dmaState(0),
+         ppiIbMaster      => dmaIbMaster(0),
+         ppiIbSlave       => dmaIbSlave(0),
+         ppiObMaster      => dmaObMaster(0),
+         ppiObSlave       => dmaObSlave(0),
+         pgpTxClk         => (others => pgpClk),
+         pgpTxClkRst      => (others => pgpClkRst),
+         pgpTxIn          => pgpTxIn,
+         pgpTxOut         => pgpTxOut,
+         pgpTxMasters     => pgpTxMasters,
+         pgpTxSlaves      => pgpTxSlaves,
+         pgpRxClk         => (others => pgpClk),
+         pgpRxClkRst      => (others => pgpClkRst),
+         pgpRxIn          => pgpRxIn,
+         pgpRxOut         => pgpRxOut,
+         pgpRxMasterMuxed => pgpRxMasterMuxed,
+         pgpRxCtrl        => pgpRxCtrl,
+         axilClk          => axilClk,
+         axilClkRst       => axilClkRst,
+         axilWriteMaster  => extAxilWriteMaster,
+         axilWriteSlave   => extAxilWriteSlave,
+         axilReadMaster   => extAxilReadMaster,
+         axilReadSlave    => extAxilReadSlave);
+
+   -------------------------------------------------------------------------------------------------
+   -- PGP GTX Array
+   -------------------------------------------------------------------------------------------------
+   PGP_GTX_GEN : for i in PGP_LANES_G-1 downto 0 generate
+      Pgp2bGtx7VarLat_1 : entity work.Pgp2bGtx7VarLat
+         generic map (
+            TPD_G                 => TPD_G,
+            STABLE_CLOCK_PERIOD_G => 4.0E-9,
+            CPLL_REFCLK_SEL_G     => "001",
+            CPLL_FBDIV_G          => PGP_GTX_CPLL_CFG_C.CPLL_FBDIV_G,
+            CPLL_FBDIV_45_G       => PGP_GTX_CPLL_CFG_C.CPLL_FBDIV_45_G,
+            CPLL_REFCLK_DIV_G     => PGP_GTX_CPLL_CFG_C.CPLL_REFCLK_DIV_G,
+            RXOUT_DIV_G           => PGP_GTX_CPLL_CFG_C.OUT_DIV_G,
+            TXOUT_DIV_G           => PGP_GTX_CPLL_CFG_C.OUT_DIV_G,
+            RX_CLK25_DIV_G        => PGP_GTX_CPLL_CFG_C.CLK25_DIV_G,
+            TX_CLK25_DIV_G        => PGP_GTX_CPLL_CFG_C.CLK25_DIV_G,
+--            PMA_RSV_G             => PMA_RSV_G,
+--            RX_OS_CFG_G           => RX_OS_CFG_G,
+--            RXCDR_CFG_G           => RXCDR_CFG_G,
+--            RXDFEXYDEN_G          => RXDFEXYDEN_G,
+--            RX_DFE_KL_CFG2_G      => RX_DFE_KL_CFG2_G,
+            TX_PLL_G              => "CPLL",
+            RX_PLL_G              => "CPLL",
+            PAYLOAD_CNT_TOP_G     => 7,
+            VC_INTERLEAVE_G       => 1,
+            NUM_VC_EN_G           => 4)
+         port map (
+            stableClk        => sysClk125,
+            gtCPllRefClk     => locRefClk,
+            gtCPllLock       => open,
+            gtQPllRefClk     => '0',
+            gtQPllClk        => '0',
+            gtQPllLock       => '0',
+            gtQPllRefClkLost => '0',
+            gtQPllReset      => open,
+            gtTxP            => dpmToRtmHsP(i),
+            gtTxN            => dpmToRtmHsM(i),
+            gtRxP            => rtmToDpmHsP(i),
+            gtRxN            => rtmToDpmHsM(i),
+            pgpTxReset       => pgpClkRst,
+            pgpTxClk         => pgpClk,
+            pgpTxRecClk      => open,
+            pgpTxMmcmReset   => open,
+            pgpTxMmcmLocked  => '1',
+            pgpRxReset       => pgpClkRst,
+            pgpRxRecClk      => open,
+            pgpRxClk         => pgpClk,
+            pgpRxMmcmReset   => open,
+            pgpRxMmcmLocked  => '1',
+            pgpRxIn          => pgpRxIn(i),
+            pgpRxOut         => pgpRxOut(i),
+            pgpTxIn          => pgpTxIn(i),
+            pgpTxOut         => pgpTxOut(i),
+            pgpTxMasters     => pgpTxMasters(i),
+            pgpTxSlaves      => pgpTxSlaves(i),
+            pgpRxMasters     => open,
+            pgpRxMasterMuxed => pgpRxMasterMuxed(i),
+            pgpRxCtrl        => pgpRxCtrl(i));
+   end generate PGP_GTX_GEN;
+
 
 end architecture STRUCTURE;
 
