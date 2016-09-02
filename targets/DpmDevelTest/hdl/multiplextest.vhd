7c1e937e24acd158fefd8b996bda148eec702d99
Larry Ruckman <ruckman@slac.stanford.edu>
2016-09-02 22:01:37 +0000

    Added SLAC licensing.
    git-svn-id: file:///afs/slac/g/reseng/svn/repos/cob_dpm/trunk/firmware@12217 0848cbd7-7569-0410-ab05-8cc0a0c67708

--- targets/DpmDevelTest/hdl/multiplextest.vhd
+++ targets/DpmDevelTest/hdl/multiplextest.vhd
@@ -1,3 +1,12 @@
+------------------------------------------------------------------------------
+-- This file is part of 'RCE Development Firmware'.
+-- It is subject to the license terms in the LICENSE.txt file found in the 
+-- top-level directory of this distribution and at: 
+--    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html. 
+-- No part of 'RCE Development Firmware', including this file, 
+-- may be copied, modified, propagated, or distributed except according to 
+-- the terms contained in the LICENSE.txt file.
+------------------------------------------------------------------------------
 --------------------------------------------------------------
 -- Serializer for High Speed I/O board (ATLAS Pixel teststand)
 -- Martin Kocian 01/2009
