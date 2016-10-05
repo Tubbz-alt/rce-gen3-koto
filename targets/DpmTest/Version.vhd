81761c304b5278b9d4c294f380829ef08d472cf7
Larry Ruckman <ruckman@slac.stanford.edu>
2016-10-05 16:16:39 +0000

    -- 10/05/2016 (0xDA10030A): Overhauled the AxiStreamDmaRead and added pending AXI read support See https://jira.slac.stanford.edu/browse/ESRCECORE-14 for testing results of this .bit file
    git-svn-id: file:///afs/slac/g/reseng/svn/repos/cob_dpm/trunk/firmware@12710 0848cbd7-7569-0410-ab05-8cc0a0c67708

--- targets/DpmTest/Version.vhd
+++ targets/DpmTest/Version.vhd
@@ -22,23 +22,24 @@ USE ieee.std_logic_1164.ALL;
 
 package Version is
 
-constant FPGA_VERSION_C : std_logic_vector(31 downto 0) := x"DA100309"; -- MAKE_VERSION
+constant FPGA_VERSION_C : std_logic_vector(31 downto 0) := x"DA10030A"; -- MAKE_VERSION
 
-constant BUILD_STAMP_C : string := "DpmTest: Vivado v2014.4 (x86_64) Built Mon Dec  7 10:17:31 PST 2015 by rherbst";
+constant BUILD_STAMP_C : string := "DpmTest: Vivado v2016.2 (x86_64) Built Wed Oct  5 08:33:52 PDT 2016 by ruckman";
 
 end Version;
 
 -------------------------------------------------------------------------------
 -- Revision History:
--- 06/26/2013 (0xDA100300): Initial Version
--- 06/26/2013 (0xDA100301): PPI
--- 06/26/2013 (0xDA100302): PPi Fix, added piplines.
--- 07/07/2013 (0xDA100303): 1Gbps PGP test
--- 07/09/2013 (0xDA100304): Flow control fix, 1Gbps
--- 08/19/2014 (0xDA100305): 5Gbps PGP test
--- 08/19/2014 (0xDA100306): 1Gbps, PPI and new BSI
--- 09/23/2014 (0xDA100307): Updates, 5Gbps
--- 10/21/2014 (0xDA100308): Tweak cdr settings and cell size for hps test.
+-- 10/05/2016 (0xDA10030A): Overhauled the AxiStreamDmaRead and added pending AXI read support
 -- 12/06/2015 (0xDA100309): Compile test.
+-- 10/21/2014 (0xDA100308): Tweak cdr settings and cell size for hps test.
+-- 09/23/2014 (0xDA100307): Updates, 5Gbps
+-- 08/19/2014 (0xDA100306): 1Gbps, PPI and new BSI
+-- 08/19/2014 (0xDA100305): 5Gbps PGP test
+-- 07/09/2013 (0xDA100304): Flow control fix, 1Gbps
+-- 07/07/2013 (0xDA100303): 1Gbps PGP test
+-- 06/26/2013 (0xDA100302): PPi Fix, added piplines.
+-- 06/26/2013 (0xDA100301): PPI
+-- 06/26/2013 (0xDA100300): Initial Version
 -------------------------------------------------------------------------------
 
