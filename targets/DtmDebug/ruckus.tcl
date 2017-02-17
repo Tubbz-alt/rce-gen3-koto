4c6f50233dae385633d5635c5017baffb306f70f
Larry Ruckman <ruckman@slac.stanford.edu>
2017-02-17 12:11:36 -0800

    adding ruckus v1.1 support

--- targets/DtmDebug/ruckus.tcl
+++ targets/DtmDebug/ruckus.tcl
@@ -3,7 +3,7 @@ source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 
 # Load common and sub-module ruckus.tcl files
 loadRuckusTcl $::env(PROJ_DIR)/../../submodules/surf
-loadRuckusTcl $::env(PROJ_DIR)/../../submodules/rce-gen3-fw-lib/DpmCore
+loadRuckusTcl $::env(PROJ_DIR)/../../submodules/rce-gen3-fw-lib/DtmCore
 
 # Load local Source Code and constraints
 loadSource -dir       "$::DIR_PATH/hdl/"
