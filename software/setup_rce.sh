6830320876315c3a27a988cd244368d19b82366b
Ryan Herbst <rherbst@slac.stanford.edu>
2017-02-27 10:29:55 -0800

    Added setup file.

--- software/setup_rce.sh
+++ software/setup_rce.sh
@@ -1,15 +1,13 @@
 
 # Setup environment
 source /mnt/host/epics/base-3.15.1/settings.sh
+source /mnt/host/rogue/setup_rce.sh
 
 # Package directories
-export ROGUE_DIR=`dirname ${PWD}/rogue`
 export SURF_DIR=`dirname ${PWD}/../submodules/surf/`
 export RCE_DIR=`dirname ${PWD}/../submodules/rce-gen3-fw-lib/`
 
 # Setup python path
-export PYTHONPATH=${ROGUE_DIR}/python:${SURF_DIR}/python:${RCE_DIR}/python:${PYTHONPATH}
+export PYTHONPATH=${SURF_DIR}/python:${RCE_DIR}/python:${PYTHONPATH}
 
-# Setup library path
-export LD_LIBRARY_PATH=${ROGUE_DIR}/lib:${LD_LIBRARY_PATH}
 
